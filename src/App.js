f21d44d5285652dbf825997cbab903a2b2c172f1
23A95A1203 <ravitejakatraju73@gmail.com>
2025-05-15 14:07:07 +0530

    Integrated Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
 import ContactMe from "./Pages/Home/ContactMe";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <div className="App">
